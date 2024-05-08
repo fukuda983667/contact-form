1eeb6e2cbab743d2581cdb877834f57ef196f6a4
fukuda <sfu983667@gmail.com>
2024-05-08 19:54:33 +0900

    二重送信対策についてコメント

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -22,6 +22,12 @@ class ContactController extends Controller
     // thanksページでページの再読み込みすると同じ内容のデータが追加される不具合がある。
     public function store(ContactRequest $request)
     {
+        // セッションを再生成している。これにより新しいcsrfトークンがフォームを含むページをクライアントに
+        // 送信したときに一緒に送信される。古いトークンは認証できなくなる。
+        // 再読み込みでform送信しても古いcsrfトークンでリクエストを送っているため認証されない。
+        // 送信ボタン連打も対策できる。一回目のリクエスト処理するときにセッションが更新され、
+        // クライアントが再度新しいcsrfトークンを得るにはフォームを含むページに移動する必要がある。
+        // 前のページに戻って再度送信ボタンを押される場合も対策できる。
         $request->session()->regenerateToken();
 
         $contact = $request->only(['name', 'email', 'tel', 'content']);
@@ -29,6 +35,7 @@ class ContactController extends Controller
         return redirect()->route('thanks');
     }
 
+    // thanksページはgetでしかリクエストを送れないため、再読み込みやリダイレクトでデータを追加することはない。
     public function thanks()
     {
         return view('thanks');
